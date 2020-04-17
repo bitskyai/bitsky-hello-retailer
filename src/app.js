26728d704cc5dbff4d0062b833e71440cb5b42a3
Shaoke <shaokexu@gmail.com>
2020-04-17 14:42:58 -0700

    Change receive conent size limit to 100mb

--- src/app.js
+++ src/app.js
@@ -20,7 +20,9 @@ async function createApp() {
     app.set("view engine", "jade");
 
     app.use(logger("dev"));
-    app.use(express.json());
+    app.use(express.json({
+      limit: "100mb"
+    }));
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, "public")));
